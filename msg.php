8d238d8258d5aaa7cd5513ee068ec022e4d651b2
Emmanuel-Dufitumukiza <73791738+Emmanuel-Dufitumukiza@users.noreply.github.com>
2021-03-17 08:28:31 +0200

    Update msg.php

--- msg.php
+++ msg.php
@@ -4,8 +4,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Message Recieved</title>
-  <link rel="stylesheet" href="app.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Message</title>
+    <link rel="stylesheet" href="app.css">
 </head>
 <body class="msg-body">
   <?php
