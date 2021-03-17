86afdfb43bd070cfcc9c0716b6f7916f50944df0
Emmanuel-Dufitumukiza <73791738+Emmanuel-Dufitumukiza@users.noreply.github.com>
2021-03-17 07:46:54 +0200

    Update tourisms.php

--- tourisms.php
+++ tourisms.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Document</title>a
+    <title>Tourisms In Rwanda</title>
     <link rel="stylesheet" href="app.css">
 </head>
 <body class="tourism-body">
